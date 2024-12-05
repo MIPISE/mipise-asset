884b44d6b16a5d7815cac54cce961c0905b3df63
StanByes <stanbyes@gmail.com>
2024-12-05 15:30:53 +0100

    :bento: Move and update assets

---
+++ assets/javascripts/app.js
@@ -0,0 +1,20 @@
+// Bootstrap
+// =============================================================================
+import * as Popper from "@popperjs/core"
+
+import 'bootstrap/js/dist/alert'
+import 'bootstrap/js/dist/button'
+// import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/offcanvas'
+//import 'bootstrap/js/dist/popover'
+import 'bootstrap/js/dist/scrollspy'
+import 'bootstrap/js/dist/tab'
+import 'bootstrap/js/dist/toast'
+import 'bootstrap/js/dist/tooltip'
+
+// Elements
+// =============================================================================
+import './elements/sidebar.js'
