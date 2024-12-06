c56b4bf8cc0093995c2de4572a5c6262bf4cd757
StanByes <stanbyes@gmail.com>
2024-12-06 10:50:41 +0100

    Remove debug

--- lib/mipise-asset/engine.rb
+++ lib/mipise-asset/engine.rb
@@ -5,7 +5,6 @@ module MipiseAsset
     initializer 'mipise.assets' do |app|
       app.config.assets.paths << MipiseAsset.stylesheets_path
       app.config.assets.paths << MipiseAsset.javascripts_path
-      p app.config.assets.paths
     end
   end
 end
