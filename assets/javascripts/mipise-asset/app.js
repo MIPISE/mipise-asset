b676c4af637f9591d0aaf0281ab94d73d5ad77a3
StanByes <stanbyes@gmail.com>
2024-12-06 12:33:01 +0100

    Move vendor into asset folder

--- assets/javascripts/mipise-asset/app.js
+++ assets/javascripts/mipise-asset/app.js
@@ -1,18 +1,18 @@
 // =============================================================================
-import * as Popper from "../../../vendor/@popperjs/core"
+import * as Popper from "../../vendor/@popperjs/core"
 
-import '../../../vendor/bootstrap/js/dist/alert'
-import '../../../vendor/bootstrap/js/dist/button'
+import '../../vendor/bootstrap/js/dist/alert'
+import '../../vendor/bootstrap/js/dist/button'
 // import '../../../vendor/bootstrap/js/dist/carousel'
-import '../../../vendor/bootstrap/js/dist/collapse'
-import '../../../vendor/bootstrap/js/dist/dropdown'
-import '../../../vendor/bootstrap/js/dist/modal'
-import '../../../vendor/bootstrap/js/dist/offcanvas'
+import '../../vendor/bootstrap/js/dist/collapse'
+import '../../vendor/bootstrap/js/dist/dropdown'
+import '../../vendor/bootstrap/js/dist/modal'
+import '../../vendor/bootstrap/js/dist/offcanvas'
 //import '../../../vendor/bootstrap/js/dist/popover'
-import '../../../vendor/bootstrap/js/dist/scrollspy'
-import '../../../vendor/bootstrap/js/dist/tab'
-import '../../../vendor/bootstrap/js/dist/toast'
-import '../../../vendor/bootstrap/js/dist/tooltip'
+import '../../vendor/bootstrap/js/dist/scrollspy'
+import '../../vendor/bootstrap/js/dist/tab'
+import '../../vendor/bootstrap/js/dist/toast'
+import '../../vendor/bootstrap/js/dist/tooltip'
 
 // Elements
 // =============================================================================
