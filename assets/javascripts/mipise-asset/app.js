4f39d855d8a7d5e713465d3cb83fb3a8a693a7fa
StanByes <stanbyes@gmail.com>
2024-12-06 13:19:19 +0100

    Remove bootstrap and popper js files and update app.js

--- assets/javascripts/mipise-asset/app.js
+++ assets/javascripts/mipise-asset/app.js
@@ -1,20 +1,26 @@
-// =============================================================================
-// require "@popperjs/core/index"
+import { loadCustomInputMasks } from "./utils/input_masks.js";
+import './elements/sidebar.js'
 
-//= require 'bootstrap/js/dist/alert'
-//= require 'bootstrap/js/dist/button'
-//= require 'bootstrap/js/dist/carousel'
-//= require 'bootstrap/js/dist/collapse'
-//= require 'bootstrap/js/dist/dropdown'
-//= require 'bootstrap/js/dist/modal'
-//= require 'bootstrap/js/dist/offcanvas'
-//= require 'bootstrap/js/dist/popover'
-//= require 'bootstrap/js/dist/scrollspy'
-//= require 'bootstrap/js/dist/tab'
-//= require 'bootstrap/js/dist/toast'
-//= require 'bootstrap/js/dist/tooltip'
+document.addEventListener('DOMContentLoaded', function () {
+  // Popper.js
+  const popperScript = document.createElement('script');
+  popperScript.src = "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js";
+  document.body.appendChild(popperScript);
 
-// Elements
-// =============================================================================
-//= require './elements/sidebar.js'
-//= require './utils/input_masks'
+  // Bootstrap
+  const bootstrapScript = document.createElement('script');
+  bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js";
+  document.body.appendChild(bootstrapScript);
+
+  const bootstrapBundleScript = document.createElement('script');
+  bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
+  bootstrapScript.integrity = "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz";
+  bootstrapScript.crossOrigin = "anonymous";
+  document.body.appendChild(bootstrapBundleScript);
+
+  // Input Mask
+  const inputMaskScript = document.createElement('script');
+  inputMaskScript.src = "https://cdn.jsdelivr.net/npm/inputmask@5.0.9/dist/inputmask.min.js"
+  inputMaskScript.onload = () => loadCustomInputMasks();
+  document.body.appendChild(inputMaskScript);
+});
