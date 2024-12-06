30e1acf60db42203435bc6e0589ec2b509fbe81b
StanByes <stanbyes@gmail.com>
2024-12-06 08:33:29 +0100

    Patch Rails module access

--- lib/rails/engine.rb
+++ lib/rails/engine.rb
@@ -2,7 +2,7 @@
 
 module MipiseAsset
   module Rails
-    class Engine < ::Rails::Engine
+    class Engine < :Rails::Engine
       initializer 'mipise.asset' do |app|
         app.config.assets.path << MipiseAsset.assets_path
       end
