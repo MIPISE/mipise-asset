93130aa4288548daf9f53a277a74fec8f4da148b
StanByes <stanbyes@gmail.com>
2024-12-06 09:04:27 +0100

    Debug

--- lib/mipise-asset/engine.rb
+++ lib/mipise-asset/engine.rb
@@ -2,6 +2,7 @@
 
 module MipiseAsset
   class Engine < ::Rails::Engine
+    p MipiseAsset.assets_path
     initializer 'mipise.assets' do |app|
       p MipiseAsset.assets_path
       app.config.assets.paths << MipiseAsset.assets_path
