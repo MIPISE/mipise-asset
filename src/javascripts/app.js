61115dc2fa2bb3d7a1b1af6c74845b01826636b5
StanByes <stanbyes@gmail.com>
2025-07-08 10:00:41 +0200

    :art: Enforce extension in react import in app.js

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -5,5 +5,5 @@ import "./elements/layout.js";
 import "./elements/dropdown-table.js";
 import "./elements/clipboard.js";
 
-import "./utils/react";
+import "./utils/react.jsx";
 import "./utils/input_masks.js";
