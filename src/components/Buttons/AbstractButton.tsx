92d2e42986228cd8d63add272779aaa0a6bc0acf
StanByes <stanbyes@gmail.com>
2025-07-28 12:39:53 +0200

    :sparkles: Add hyperlink prop in AbstractButton

--- src/components/Buttons/AbstractButton.tsx
+++ src/components/Buttons/AbstractButton.tsx
@@ -1,33 +1,32 @@
-import React, { ReactElement } from "react";
+import React, {Fragment, ReactElement} from "react";
 import {Colors, GlobalProps, Size} from "../types";
 
 export type AbstractButtonProps = GlobalProps & {
-    children: ReactElement[] | string
-    color: Colors
-    size: Size
-    rounded?: number
-    variant?: "link" | "subtle"
+  children: ReactElement[] | string
+  color: Colors
+  size: Size
+  hyperlink?: boolean
+  rounded?: number
+  variant?: "link" | "subtle"
 }
 
 const AbstractButton: React.FC<AbstractButtonProps>
-    = ({ color, size, variant, rounded, children, ...props }) => {
+  = ({ color, size, hyperlink = false, variant, rounded, children, ...props }) => {
+    let content;
+    if (Array.isArray(children) || React.isValidElement(children)) {
+      content = children;
+    } else {
+      content = <span dangerouslySetInnerHTML={{ __html: children }} />;
+    }
 
-        let content;
-        if (Array.isArray(children) || React.isValidElement(children)) {
-            content = children;
-        } else {
-            content = <span dangerouslySetInnerHTML={{ __html: children }} />;
-        }
-
-        return (
-            <button
-                className={`btn btn-${color}${(variant ? `-${variant}` : "")}${size ? ` btn-${size}` : ""}${rounded ? ` rounded-${rounded}` : ""} ${props.classes}`} {...props}>
-                <span
-                    className="" {...props}>
-                    {content}
-                </span>
-            </button>
-        );
-    };
+    const classes = `btn btn-${color}${(variant ? `-${variant}` : "")}${size ? ` btn-${size}` : ""}${rounded ? ` rounded-${rounded}` : ""} ${props.classes}`;
+    return (
+      <Fragment>
+        {hyperlink
+          ? <a className={classes} {...props}>{content}</a>
+          : <button className={classes} {...props}>{content}</button>}
+      </Fragment>
+    );
+  };
 
 export default AbstractButton;
