64e050dc18d44b1c886e4f0d9bd93fbb66c99b9c
StanByes <stanbyes@gmail.com>
2025-07-28 12:40:20 +0200

    :sparkles: Rework react.jsx engine to better handle DOM/React elements

--- src/javascripts/utils/react.jsx
+++ src/javascripts/utils/react.jsx
@@ -1,52 +1,76 @@
 // React Helper
 import React from "react";
-import { renderToString } from "react-dom/server";
+import { createRoot } from "react-dom/client";
 
-const renderComponent = (component, index, pathKey) => {
-    const props = {};
-    component.getAttributeNames().forEach((name) => {
-        if (name === "data-component")
-            return;
+/**
+ * @param element Element
+ * @param index number
+ * @param pathKey string
+ */
+const renderElement = (element, index, pathKey) => {
+    if (element.nodeType === Node.COMMENT_NODE)
+        return "";
 
-        let prop = component.getAttribute(name);
-        if (prop === "")
-            prop = true;
+    if (element.nodeType === Node.TEXT_NODE)
+        return element.textContent;
 
-        props[name.replace("data-", "")] = prop;
+    const attributes = Array.from(element.attributes);
+    const children = Array.from(element.childNodes).map((child, i) => {
+        return renderElement(child, i, `${pathKey}_${i}`)
     });
 
-    const componentName = props["name"];
-    const currentKey = `${pathKey}_${componentName}_${index}`;
+    const props = {};
+    if (element.hasAttribute("data-component")) {
+        // React component
+        attributes.forEach(attr => {
+            if (attr.name === "data-component")
+                return;
 
-    const childrenComponent = Array.from(component.querySelectorAll(":scope > div[data-component]"));
-    const children = [];
-    if (childrenComponent.length !== 0) {
-        for (const i in childrenComponent) {
-            children.push(renderComponent(childrenComponent[i], i, currentKey));
-        }
+            let prop = attr.value;
+            if (prop === "")
+                prop = true;
+
+            props[attr.name.replace("data-", "")] = prop;
+        });
+
+        const componentName = props["name"];
+        props["children"] = children;
+
+        const ComponentFunction = require(`../../components/${componentName}.tsx`).default;
+        return (
+            <ComponentFunction {...props} key={pathKey}/>
+        );
+    } else {
+        // DOM element
+        attributes.forEach(attr => {
+            if (attr.name === "class")
+                return;
+
+            let prop = attr.value;
+            if (prop === "")
+                prop = true;
+
+            props[attr.name] = prop;
+        });
+
+        if (element.className)
+            props.className = element.className;
+
+        if (element.tagName.toLowerCase() === "img")
+            return React.createElement(element.tagName.toLowerCase(), { ...props, key: `${pathKey}_${element.tagName}_${index}}`});
+
+        return React.createElement(element.tagName.toLowerCase(), { ...props, key: `${pathKey}_${element.tagName}_${index}}`}, children);
     }
-    props["children"] = children.length > 0 ? children : component.innerHTML;
-
-    const ComponentFunction = require(`../../components/${componentName}.tsx`).default;
-    return (
-        <ComponentFunction {...props} key={currentKey}/>
-    );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const components = document.querySelectorAll("div[data-component]");
     components.forEach((component, i) => {
         const key = component.getAttribute("data-name");
-        const renderedComponent = renderComponent(component, i, key);
-        const htmlString = renderToString(renderedComponent);
+        const rendered = renderElement(component, i, `${key}_${i}`);
 
-        const previousElement = component.previousElementSibling;
-        if (previousElement) {
-            previousElement.insertAdjacentHTML("afterend", htmlString);
-        } else {
-            component.parentElement.insertAdjacentHTML("beforeend", htmlString);
-        }
-
-        component.remove();
+        component.innerHTML = "";
+        const root = createRoot(component);
+        root.render(rendered);
     });
 });
