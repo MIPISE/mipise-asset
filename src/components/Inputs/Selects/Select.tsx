539791fdade20535398b3a016cefda44c07dcb49
StanByes <stanbyes@gmail.com>
2025-08-01 14:26:59 +0200

    :recycle: Remove JSON parse for collection Select and clean code

--- src/components/Inputs/Selects/Select.tsx
+++ src/components/Inputs/Selects/Select.tsx
@@ -1,8 +1,9 @@
-import React, {ReactElement, useEffect, useRef, useState} from "react";
-import { GlobalProps } from "../../types";
+import React, {useEffect, useRef, useState} from "react";
 import SelectItem from "./SelectItem";
 import optionalManagement from "../optionalManagement";
-import OptionalText from "../OptionalText";
+import parseInputProps from "../parseInputProps";
+import {AbstractInputProps} from "../AbstractInput";
+import Label from "../Label";
 
 export type SelectOption = {
   label: string;
@@ -10,45 +11,23 @@ export type SelectOption = {
   selected?: boolean
 };
 
-export type SelectProps = GlobalProps & {
-  attribute: string;
-  collection: string | SelectOption[];
-  objectName?: string;
-  label?: ReactElement[] | string;
-  placeholder?: ReactElement[] | string;
-  required?: boolean;
+export type SelectProps = AbstractInputProps & {
+  collection: SelectOption[],
+  includeBlank?: boolean
 };
 
-const Select: React.FC<SelectProps> = ({attribute, collection, objectName = "user", label, placeholder, required = false, ...props}) => {
+const Select: React.FC<SelectProps> = ({collection, required, includeBlank, ...props}) => {
   const selectRef = useRef(null);
   const [isRequired, setIsRequired] = useState(required || false);
 
-  const name = `${objectName}[${attribute}]`;
-  const id = `${objectName}_${attribute}`;
+  const {name, id, placeholder} = parseInputProps(props);
 
-  const displayLabel =
-    label || attribute.charAt(0).toUpperCase() + attribute.slice(1);
-
-  const attributeClass = `select--${attribute.replace(/[^a-zA-Z0-9_-]/g, "-")}`;
-
-  const effectivePlaceholder = placeholder || displayLabel;
-
-  if (typeof collection == "string") {
-    if (!collection.startsWith("[")) {
-      console.warn("Collection is not an array");
-      return;
-    }
-
-    collection = JSON.parse(collection) as SelectOption[];
-  }
-
+  const attributeClass = `select--${props.attribute.replace(/[^a-zA-Z0-9_-]/g, "-")}`;
   useEffect(optionalManagement(selectRef, setIsRequired), []);
 
   return (
     <div className={`form-group ${props.classes}`}>
-      <label htmlFor={id} className="form-label">
-        {displayLabel} {!isRequired && <OptionalText/>}
-      </label>
+      <Label isRequired={isRequired} {...props}/>
       <select
         id={id}
         name={name}
@@ -57,9 +36,11 @@ const Select: React.FC<SelectProps> = ({attribute, collection, objectName = "use
         defaultValue={collection.find(c => c.selected)?.value || ""}
         ref={selectRef}
       >
-        <option value="" disabled>
-          {effectivePlaceholder}
-        </option>
+        {includeBlank &&
+          <option value="" disabled>
+            {placeholder}
+          </option>
+        }
         {collection.map((item: SelectOption, idx: number) => (
           <SelectItem key={idx} {...item} />
         ))}
