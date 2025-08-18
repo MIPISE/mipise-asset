de25432b03fac38af3b4089f4de2e6ae558b8390
StanByes <stanbyes@gmail.com>
2025-08-18 10:19:13 +0200

    :art: Add buttonType and visuallyHidden props in IconButton

--- src/components/Buttons/IconButton.tsx
+++ src/components/Buttons/IconButton.tsx
@@ -6,18 +6,22 @@ import Icon from "../Icon";
 export type IconButtonProps = AbstractButtonProps & {
   icon: Icons
   iconEnd: boolean
+  buttonType?: "submit" | "reset" | "button"
   iconType?: "rs" | "ss"
   square?: boolean
+  visuallyHidden?: boolean
 }
 
 const IconButton: React.FC<IconButtonProps>
-  = ({ color, size, variant, children, icon, iconType = "rs", iconEnd, square, rounded, classes = "", ...props }) => {
+  = ({ color, size, variant, children, icon, iconType = "rs", iconEnd, square, rounded, visuallyHidden, buttonType = "submit", classes = "", ...props }) => {
     return (
       <button
-        className={`btn btn-${color}${(variant ? `-${variant}` : "")} ${size ? `btn-${size}` : ""} ${square ? `btn-square` : "d-inline-flex align-items-center justify-content-center"}${rounded ? ` rounded-${rounded}` : ""} ${classes} `}{...props}>
+        className={`btn btn-${color}${(variant ? `-${variant}` : "")} ${size ? `btn-${size}` : ""} ${square ? `btn-square` : "d-inline-flex align-items-center justify-content-center"}${rounded ? ` rounded-${rounded}` : ""} ${classes} `}{...props}
+        type={buttonType}
+      >
         <Icon icon={icon} type={iconType} classes={`${iconEnd ? `${square ? "" : "ps-2"} order-2` : "pe-2"}`}/>
         <span
-          className="" {...props}>
+          className={visuallyHidden ? "visually-hidden" : null}>
           {children}
         </span>
       </button>
