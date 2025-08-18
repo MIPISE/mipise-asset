b8838cf62095fb130b24b72a888697dc3f7ba2ac
StanByes <stanbyes@gmail.com>
2025-08-18 10:19:44 +0200

    :art: Update size and rounded props and add type in AbstractButton

--- src/components/Buttons/AbstractButton.tsx
+++ src/components/Buttons/AbstractButton.tsx
@@ -3,21 +3,22 @@ import {Color, GlobalProps, Size} from "../types";
 
 export type AbstractButtonProps = GlobalProps & {
   color: Color
-  size: Size
+  size?: Size
   hyperlink?: boolean
-  rounded?: number
+  rounded?: string
   square?: boolean
   variant?: "link" | "subtle"
+  type?: "submit" | "reset" | "button"
 }
 
 const AbstractButton: React.FC<AbstractButtonProps>
-  = ({ color, size, hyperlink = false, variant, rounded, square = false, classes = "", children, ...props }) => {
+  = ({ color, size, hyperlink = false, variant, rounded, square = false, type, classes = "", children, ...props }) => {
     const completeClasses = `btn btn-${color}${(variant ? `-${variant}` : "")}${size ? ` btn-${size}` : ""}${square ? " btn-square" : ""}${rounded ? ` rounded-${rounded}` : ""} ${classes}`;
     return (
       <Fragment>
         {hyperlink
           ? <a className={completeClasses} {...props}>{children}</a>
-          : <button className={completeClasses} {...props}>{children}</button>}
+          : <button className={completeClasses} type={type} {...props}>{children}</button>}
       </Fragment>
     );
   };
