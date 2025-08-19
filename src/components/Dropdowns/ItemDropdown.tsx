be97c98f1539ddd7b506c86b67e1143f358c17ac
StanByes <stanbyes@gmail.com>
2025-08-19 13:45:17 +0200

    :art: Allow ItemDropdown being a submit button

--- src/components/Dropdowns/ItemDropdown.tsx
+++ src/components/Dropdowns/ItemDropdown.tsx
@@ -3,16 +3,24 @@ import {GlobalProps} from "../types";
 
 export type ItemDropdownProps = GlobalProps & {
   link: string,
-  inNav?: boolean
+  inNav?: boolean,
+  type?: "button" | "submit"
 }
 
 const ItemDropdown: React.FC<ItemDropdownProps>
-  = ({ children, link, inNav, classes = "", ...props }) => {
+  = ({ children, link, inNav, type = "button", classes = "", ...props }) => {
+    const className = `${inNav ? "nav-link" : "dropdown-item"} ${classes}`;
+
     return (
       <li>
-        <a className={`${inNav ? "nav-link" : "dropdown-item"} ${classes}`} href={link} type="button" aria-label="" {...props}>
-          {children}
-        </a>
+        {type == "submit"
+          ? <button className={className} type={"submit"} {...props}>
+              {children}
+            </button>
+          : <a className={className} href={link} aria-label="" {...props}>
+              {children}
+            </a>
+        }
       </li>
     );
   };
