50c876579b1fe385d8e8e9b39fa7ea18fae3282f
StanByes <stanbyes@gmail.com>
2025-09-11 10:23:38 +0200

    :art: Add rightIcon for nav link

--- src/components/Navbar/NavbarLink.tsx
+++ src/components/Navbar/NavbarLink.tsx
@@ -6,15 +6,24 @@ import { Icons } from "../icons";
 type NavbarLinkProps = GlobalProps & {
   link: string,
   label: string,
-  icon?: Icons
+  icon?: Icons,
+  rightIcon?: Icons,
+  rightIconClasses?: string
 }
 
-const NavbarLink: React.FC<NavbarLinkProps> =
-  ({link, label, icon, ...props}) => {
+const NavbarLink: React.FC<NavbarLinkProps> = ({
+  link,
+  label,
+  icon,
+  rightIcon,
+  rightIconClasses,
+  ...props
+}) => {
     return (
       <a className={`nav-link ${props.classes}`} href={link}>
         {icon && <Icon icon={icon}/>}
         {label}
+        {rightIcon && <Icon icon={rightIcon} classes={rightIconClasses}/>}
       </a>
     )
   };
