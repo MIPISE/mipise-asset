fa7ae89338406269f3fb297011f65b3e57d9b4b8
StanByes <stanbyes@gmail.com>
2025-10-02 10:33:45 +0200

    :art: Clean ClipboardBadge and handle copy on button click

--- src/components/Badges/ClipboardBadge.tsx
+++ src/components/Badges/ClipboardBadge.tsx
@@ -1,27 +1,39 @@
 import React from "react";
-import { Color, GlobalProps, Size } from "../types";
+import { Color, Size } from "../types";
+import {AbstractBadgeProps} from "./AbstractBadge";
 
-export type ClipboardBadgeProps = GlobalProps & {
-    color: Color,
-    size: Size,
-    sizeClipboard?: Size,
-    colorClipboard?: Color,
-    variant?: string,
-    rounded?: string,
+export type ClipboardBadgeProps = AbstractBadgeProps & {
+    sizeClipboard?: Size
+    colorClipboard?: Color
 }
 
-const ClipboardBadge: React.FC<ClipboardBadgeProps>
-    = ({ color, size, variant, children, rounded, sizeClipboard = Size.EXTRASMALL, colorClipboard, classes = "", ...props }) => {
-        return (
-            <div className={`badge badge-${color}${size ? ` badge-${size}` : ""}${variant ? ` badge-${variant}` : ""}${rounded ? ` badge-${rounded}` : ""} ${classes}`} {...props}>
-                <span className={`textToCopy`}>
-                    {children}
-                </span>
-                <button className={`copyButton btn btn-${colorClipboard} btn-${sizeClipboard} btn-square ms-1`}>
-                    <i className="fi fi-rs-copy"></i>
-                </button>
-            </div>
-        );
-    };
+const ClipboardBadge: React.FC<ClipboardBadgeProps> = ({
+  color,
+  size,
+  variant,
+  children,
+  rounded,
+  sizeClipboard = Size.EXTRASMALL,
+  colorClipboard,
+  classes = "",
+  ...props
+}) => {
+  const text = typeof children == "string" ? children : "";
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(text);
+  }
+
+  return (
+    <div className={`badge badge-${color}${size ? ` badge-${size}` : ""}${variant ? ` badge-${variant}` : ""}${rounded ? ` badge-${rounded}` : ""} ${classes}`} {...props}>
+      <span className={`textToCopy`}>
+        {children}
+      </span>
+      <button className={`copyButton btn btn-${colorClipboard} btn-${sizeClipboard} btn-square ms-1`} onClick={handleCopy}>
+        <i className="fi fi-rs-copy"></i>
+      </button>
+    </div>
+  );
+};
 
 export default ClipboardBadge;
