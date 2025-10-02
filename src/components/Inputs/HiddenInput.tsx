e941a0b03c0accaa36325e6d754f3c44f88e7dbc
StanByes <stanbyes@gmail.com>
2025-10-02 12:53:57 +0200

    :art: Add autocomplete to off in HiddenInput

--- src/components/Inputs/HiddenInput.tsx
+++ src/components/Inputs/HiddenInput.tsx
@@ -5,7 +5,7 @@ import InputItem from "./InputItem";
 type HiddenInputProps = Omit<AbstractInputProps, "type">
 
 const HiddenInput: React.FC<HiddenInputProps> = ({ ...props }) => {
-  return <InputItem type="hidden" {...props}/>
+  return <InputItem type="hidden" autoComplete="off" {...props}/>
 }
 
 export default HiddenInput;
