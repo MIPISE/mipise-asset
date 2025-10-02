7923f80056dfe739fbd2eeae6a9e28fbb5ca9682
StanByes <stanbyes@gmail.com>
2025-10-02 12:54:07 +0200

    :art: Add hidden input for CheckboxInput.tsx

--- src/components/Inputs/CheckboxInput.tsx
+++ src/components/Inputs/CheckboxInput.tsx
@@ -3,6 +3,7 @@ import optionalManagement from "./optionalManagement";
 import {AbstractInputProps} from "./AbstractInput";
 import Label from "./Label";
 import InputItem from "./InputItem";
+import HiddenInput from "./HiddenInput";
 
 const CheckboxInput: React.FC<AbstractInputProps> = ({
   required,
@@ -17,6 +18,7 @@ const CheckboxInput: React.FC<AbstractInputProps> = ({
   return (
     <div className={`form-group ${props.classes}`}>
       <div className="form-check">
+        <HiddenInput {...props} />
         <InputItem type="checkbox" ref={inputRef} inputHtmlProps={{classes: "form-check-input"}} defaultChecked={checked} {...props} />
         <Label classes="form-check-label" isRequired={isRequired} {...props}/>
         {hint && <div
