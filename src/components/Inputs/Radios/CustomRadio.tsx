3daaa65af696fcf23597a64f570c29864229a070
gary <fernandes.gary78690@gmail.com>
2025-12-08 15:33:16 +0100

    :lipstick: Gestion disable for customRadio

--- src/components/Inputs/Radios/CustomRadio.tsx
+++ src/components/Inputs/Radios/CustomRadio.tsx
@@ -1,29 +1,50 @@
-import React, {ReactNode} from "react";
+import React, { ReactNode } from "react";
 import { GlobalProps } from "../../types";
-import CustomRadioItem, {CustomRadioItemProps} from "./CustomRadioItem";
-import {formatId, formatName} from "../parseInputProps";
+import CustomRadioItem, { CustomRadioItemProps } from "./CustomRadioItem";
+import { formatId, formatName } from "../parseInputProps";
 
 export type CustomRadioProps = GlobalProps & {
-  attribute: string
-  items: CustomRadioItemProps[]
-  legend: ReactNode
-  objectName: string
-}
+  attribute: string;
+  items: CustomRadioItemProps[];
+  legend: ReactNode;
+  objectName: string;
+  disabled?: boolean;
+};
 
-const CustomRadio: React.FC<CustomRadioProps>
-  = ({ attribute, items, legend, objectName, classes = "" }) => {
-    return (
-      <div className={`form-group ${formatId(objectName, attribute)} radio_buttons ${classes}`}>
-        <legend
-          className="form-label">
-          {legend}
-        </legend>
-        <input type="hidden" name={formatName(objectName, attribute)} autoComplete="off"/>
-        {items.map(i => {
-          return <CustomRadioItem attribute={attribute} objectName={objectName} {...i}/>
-        })}
-      </div>
-    );
-  };
+const CustomRadio: React.FC<CustomRadioProps> = ({
+  attribute,
+  items,
+  legend,
+  objectName,
+  classes = "",
+  disabled = false,
+}) => {
+  return (
+    <div
+      className={`form-group ${formatId(objectName, attribute)} radio_buttons ${classes}`}
+    >
+      <legend className={`form-label ${disabled ? "text-muted" : ""}`}>
+        {legend}
+      </legend>
+
+      <input
+        type="hidden"
+        name={formatName(objectName, attribute)}
+        autoComplete="off"
+      />
+      {items.map((i, index) => {
+        return (
+          <CustomRadioItem
+            key={index}
+            attribute={attribute}
+            objectName={objectName}
+            disabled={disabled}
+            {...i}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CustomRadio;
--- src/components/Inputs/Radios/CustomRadioItem.tsx
+++ src/components/Inputs/Radios/CustomRadioItem.tsx
@@ -1,34 +1,54 @@
 import React from "react";
-import {GlobalProps} from "../../types";
+import { GlobalProps } from "../../types";
 import Icon from "../../Icon";
-import {Icons} from "../../icons";
-import {formatId, formatName} from "../parseInputProps";
+import { Icons } from "../../icons";
+import { formatId, formatName } from "../parseInputProps";
 
 export type CustomRadioItemProps = GlobalProps & {
-  attribute: string
-  label: string
-  objectName: string
-  selected: boolean
-  value: string
-  icon?: Icons
-}
+  attribute: string;
+  label: string;
+  objectName: string;
+  selected: boolean;
+  value: string;
+  icon?: Icons;
+  disabled?: boolean;
+};
 
-const CustomRadioItem: React.FC<CustomRadioItemProps>
-  = ({ attribute, label, objectName, selected, value, icon, classes = "", ...props }) => {
-    const id = `${formatId(objectName, attribute)}_${value}`;
+const CustomRadioItem: React.FC<CustomRadioItemProps> = ({
+  attribute,
+  label,
+  objectName,
+  selected,
+  value,
+  icon,
+  classes = "",
+  disabled = false,
+  ...props
+}) => {
+  const id = `${formatId(objectName, attribute)}_${value}`;
 
-    return (
-      <>
-        <input
-          className={`btn-check visually-hidden ${classes}`} type="radio"
-          name={formatName(objectName, attribute)} id={id} defaultChecked={selected} autoComplete="off" value={value}/>
-        <label
-          className={`${icon ? "btn btn-light btn-radio d-flex flex-wrap flex-column align-items-center py-3" : "btn btn-light me-1 mt-0"}`} {...props} htmlFor={id}>
-          {icon && <Icon icon={icon} classes={`fs-3 text-primary me-2`}/>}
-          {label}
-        </label>
-      </>
-    );
-  };
+  return (
+    <>
+      <input
+        className={`btn-check visually-hidden ${classes}`}
+        type="radio"
+        name={formatName(objectName, attribute)}
+        id={id}
+        defaultChecked={selected}
+        autoComplete="off"
+        value={value}
+        disabled={disabled}
+      />
+      <label
+        className={`${icon ? "btn btn-light btn-radio d-flex flex-wrap flex-column align-items-center py-3" : "btn btn-light me-1 mt-0"}`}
+        {...props}
+        htmlFor={id}
+      >
+        {icon && <Icon icon={icon} classes={`fs-3 text-primary me-2`} />}
+        {label}
+      </label>
+    </>
+  );
+};
 
 export default CustomRadioItem;
