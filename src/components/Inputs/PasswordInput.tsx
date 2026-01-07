40881826608fc0deafd6533a5f5203a0af2ff895
gary <fernandes.gary78690@gmail.com>
2026-01-07 14:18:42 +0100

    Error phoneInput

--- src/components/Inputs/PasswordInput.tsx
+++ src/components/Inputs/PasswordInput.tsx
@@ -1,54 +1,48 @@
 import React, { useEffect, useRef, useState } from "react";
 import { AbstractInputProps } from "./AbstractInput";
-import Icon from "../Icon";
-import { Icons } from "../icons";
 import Label from "./Label";
 import InputItem from "./InputItem";
 import optionalManagement from "./optionalManagement";
 import parseInputProps from "./parseInputProps";
 
-type PasswordInputProps = Omit<AbstractInputProps, "type"> & { error?: string };
+type PhoneInputProps = Omit<AbstractInputProps, "type"> & { error?: string };
 
-const PasswordInput: React.FC<PasswordInputProps> = ({
+const PhoneInput: React.FC<PhoneInputProps> = ({
   required,
   error,
   classes = "",
+  label,
   ...props
 }) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [isRequired, setIsRequired] = useState(required || false);
 
-  const [showed, setShowed] = useState(false);
-  const handleShowClick = () => setShowed(!showed);
-
   useEffect(optionalManagement(inputRef, setIsRequired), []);
 
   const { id } = parseInputProps(props);
 
-  const hasLabel = typeof props.label === "string" && props.label.trim() !== "";
+  const safeLabel = label === "false" ? "" : label;
+  const hasLabel = typeof safeLabel === "string" && safeLabel.trim() !== "";
 
   const computedInputClasses = `${classes} ${error ? "is-invalid" : ""}`.trim();
 
   return (
-    <div className={`form-group ${id} password ${classes}`}>
-      {hasLabel && <Label isRequired={isRequired} {...props} />}
+    <div className={`form-group ${id} phone-input`}>
+      {hasLabel && (
+        <Label isRequired={isRequired} label={safeLabel} {...props} />
+      )}
 
       <div className="input-group has-validation">
         <InputItem
-          type={showed ? "text" : "password"}
+          type="tel"
+          pattern="^\d{8,15}$"
+          inputMode="numeric"
+          data-behavior="digits-only"
           ref={inputRef}
           required={isRequired}
           classes={computedInputClasses}
           {...props}
         />
-        <button
-          className="btn btn-primary btn-square rounded-end"
-          type="button"
-          onClick={handleShowClick}
-          tabIndex={-1}
-        >
-          <Icon icon={showed ? Icons.HIDE_PASSWORD : Icons.SHOW_PASSWORD} />
-        </button>
 
         {error && <div className="invalid-feedback">{error}</div>}
       </div>
@@ -58,4 +52,4 @@ const PasswordInput: React.FC<PasswordInputProps> = ({
   );
 };
 
-export default PasswordInput;
+export default PhoneInput;
